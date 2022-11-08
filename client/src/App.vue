db51628877b0f219d997d84051a3478d7234c403
M.Yusril Ihza <ihza212325@gmail.com>
2022-11-09 05:51:41 +0700

    setup awal development vue

---
+++ client/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+// import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <h1 class="text-center font-bold text-4xl">LONTONG</h1>
+
+  <!-- <RouterView /> -->
+</template>
